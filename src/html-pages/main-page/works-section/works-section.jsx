2a23625a3e81822ccd011a45730ea37e2e1e9df2
Maxim93mk <kinjashov.m@rambler.ru>
2024-04-21 15:25:01 +0300

    main-page works-section заголовок, список переходов готово

--- src/html-pages/main-page/works-section/works-section.jsx
+++ src/html-pages/main-page/works-section/works-section.jsx
@@ -6,6 +6,13 @@ function WorksSection() {
     return (
         <>
             <section className='works'>
+                <h2 className='works-title'>Наши работы</h2>
+                <ul className='works-nav'>
+                    <li><a href='#' className='works-nav-links'>Показать все</a></li>
+                    <li><a href="#" className='works-nav-links'>Парикмахерские услуги</a></li>
+                    <li><a href="#" className='works-nav-links'>Маникюр</a></li>
+                    <li><a href="#" className='works-nav-links'>Педикюр</a></li>
+                </ul>
             </section>
         </>
     );
